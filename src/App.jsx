e2c6fc12b8f6729331fa7326fedbdab546050fb2
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-02-25 19:49:32 +0200

    create Edusity-Hero Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 // import React from "react";
+import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Hero />
     </div>
   );
 };

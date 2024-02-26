5417633127bae8c1f93f71edde1c8806ea097fc4
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-02-26 11:47:04 +0200

    create programs component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 // import React from "react";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
+import Programs from "./Components/Programs/Programs";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <div className="container">
+        <Programs />
+      </div>
     </div>
   );
 };

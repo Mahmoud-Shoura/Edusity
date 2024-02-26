66fe75420eabd0d70e40a3f81d5092604155df57
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-02-26 14:13:47 +0200

    create Testimonials component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Campus from "./Components/Campus/Campus";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Programs from "./Components/Programs/Programs";
+import Testimonials from "./Components/Testimonials/Testimonials";
 import Title from "./Components/Title/Title";
 
 const App = () => {
@@ -17,6 +18,8 @@ const App = () => {
         <About />
         <Title subTitle="Gallery" title="Campus Photos" />
         <Campus />
+        <Title subTitle="TESTIMONIALS" title="What Student Says" />
+        <Testimonials />
       </div>
     </div>
   );

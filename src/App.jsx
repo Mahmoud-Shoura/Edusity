12c421e042a0d0b624822efa37adc23a1cdcc2f6
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-02-26 12:55:15 +0200

    Edit to Navbar Dynamic className dark-nav

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Title subTitle="Our PROGRAM" title="What We Offer" />
         <Programs />
         <About />
+        <Title subTitle="Gallery" title="Campus Photos" />
       </div>
     </div>
   );

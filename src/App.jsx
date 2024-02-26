02c2a7ca473c99e8d0977225ec28def90719eb45
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-02-26 13:11:05 +0200

    create Campus component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import React from "react";
 import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Programs from "./Components/Programs/Programs";
@@ -15,6 +16,7 @@ const App = () => {
         <Programs />
         <About />
         <Title subTitle="Gallery" title="Campus Photos" />
+        <Campus />
       </div>
     </div>
   );

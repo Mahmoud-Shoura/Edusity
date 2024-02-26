4d2892b1f85dfea08785d089af6c0e61a79d1532
mahmoud shoura <mahmoudhameidshoura@gmail.com>
2024-02-26 16:25:28 +0200

    create Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import About from "./Components/About/About";
 import Campus from "./Components/Campus/Campus";
 import Contact from "./Components/Contact/Contact";
+import { Footer } from "./Components/Footer/Footer";
 import Hero from "./Components/Hero/Hero";
 import Navbar from "./Components/Navbar/Navbar";
 import Programs from "./Components/Programs/Programs";
@@ -23,6 +24,7 @@ const App = () => {
         <Testimonials />
         <Title subTitle="Contact Us" title="Get in Touch" />
         <Contact />
+        <Footer />
       </div>
     </div>
   );
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <div className="footer">
+      <p>© 2024 Edusity. All rights reserved.</p>
+      <ul>
+        <li>Terms of services</li>
+        <li>Privacy Policy</li>
+      </ul>
+    </div>
+  );
+};
